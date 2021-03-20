5a0d6c693b6a0afab33933ea92675e6400ab20c9
Artem Shabanov <itemashabanov@gmail.com>
2021-03-20 13:38:08 +0200

    Get MeetupTitle by meetup ID

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,37 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+const a = new Vue({
+    data() {
+        return {
+            meetupCount: 5,
+            meetupID: null,
+            firstTitle: null,
+        }
+    },
+
+    watch: {
+        meetupID() {
+            this.title = this.getMeetupTitle(this.meetupID);
+        }
+    },
+
+    computed: {
+        title: {
+            get: function () {
+                return this.firstTitle;
+            },
+
+            set: function (newValue) {
+                this.firstTitle = newValue;
+            },
+        }
+    },
+
+    methods: {
+        getMeetupTitle(id) {
+            fetch("https://course-vue.javascript.ru/api/meetups/" + id)
+                .then(res => res.json())
+                .then(meetup => this.firstTitle = meetup.title);
+        },
+    },
+}).$mount("#meetupTitle");
