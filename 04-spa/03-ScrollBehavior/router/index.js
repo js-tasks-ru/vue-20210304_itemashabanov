f3bf48d736abb6ef7ff96a2db98d5b5f366322ea
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-04-10 21:09:43 +0300

    Added task ScrollBehavior

---
+++ 04-spa/03-ScrollBehavior/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+Vue.use(VueRouter);
+
+// export function scrollBehavior(to, from, savedPosition) {}
+
+export const router = new VueRouter({
+  mode: 'history',
+
+  base: '/04-SPA/03-ScrollBehavior',
+
+  // scrollBehavior,
+
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      component: () => import('../views/MeetupsPage'),
+    },
+    {
+      path: '/meetups',
+      name: 'meetups',
+      component: () => import('../views/MeetupsPage'),
+    },
+    {
+      path: '/meetups/:meetupId(\\d+)',
+      name: 'meetup',
+      props: true,
+      redirect: (to) => ({ name: 'meetup.description', params: to.params }),
+      meta: {
+        showReturnToMeetups: true,
+        saveScrollPosition: true,
+      },
+      component: () => import('../views/MeetupPage'),
+      children: [
+        {
+          path: '',
+          alias: 'description',
+          name: 'meetup.description',
+          props: true,
+          component: () => import('../views/MeetupDescriptionPage'),
+        },
+        {
+          path: 'agenda',
+          name: 'meetup.agenda',
+          props: true,
+          component: () => import('../views/MeetupAgendaPage'),
+        },
+      ],
+    },
+  ],
+});
